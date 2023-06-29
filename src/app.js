82a0e88ace1e56fc67ed69be65d024c299347e5b
Pedro Nascimento <sn.jaypee@gmail.com>
2023-06-29 16:24:15 -0300

    feat: add POST /participants WITHOUT validation

--- src/app.js
+++ src/app.js
@@ -1,17 +1,53 @@
 import Express from 'express';
 import 'dotenv/config'
 import { MongoClient } from 'mongodb';
+import dayjs from 'dayjs';
 
 const app = Express();
+app.use(Express.json());
+app.listen(5000);
+
 const mClient = new MongoClient(process.env.DATABASE_URL);
+await mClient.connect();  // No error-treatment cuz i want the process to crash if this goes wrong!.
 const db = mClient.db();
+
+console.log("Servidor iniciado na porta 5000");
+
+//
+// Endpoints section
+//
 
 app.get("/participants", (req, res) => {
 
 })
 
-app.post("/participants", (req, res) => {
-    
+app.post("/participants", async (req, res) => {
+    const { name } = req.body;
+    // validations
+
+    try {
+        const participantsArray = await db.collection("participants").find({"name": name}).toArray();
+        if(participantsArray.length !== 0) return res.sendStatus(409);
+
+        db.collection("participants").insertOne({
+            name,
+            lastStatus: Date.now()
+        });
+
+        db.collection("messages").insertOne({
+            from: name,
+            to: "Todos",
+            text: "entra na sala...",
+            type: "status",
+            time: dayjs().format("HH:mm:ss"),
+        })
+
+        return res.sendStatus(201);
+
+    } catch (error) {
+        res.sendStatus(500);
+        console.log(error.message);
+    }
 })
 
 app.get("/messages", (req, res) => {
